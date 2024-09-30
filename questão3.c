2b2166bf0df8b28ef418bb32bf5ad32c03c6e839
Maria Milena <necimalaquias20@gmail.com>
2024-09-30 20:08:49 -0300

    Calculadora
    implementação de 16 bits após o número float ou double

---
+++ questão3.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+/*Autor: Maria Milena Neci Malaquias
+Email: milena.nmalaquias@upe.br
+*/
+
+
+void Float(float num) {
+    int bits;
+    //armazenabits
+    memcpy(&bits, &num, sizeof(float));
+
+    // Sinal 0 ou 1 para negativo e positivo
+    int sinal = (bits >> 31) & 0x1;
+
+    // Expoente
+    int expoente = (bits >> 23) & 0xFF;
+
+    // Fração
+    int fracao = bits & 0x7FFFFF;
+
+    // Expoente com viés (bias de 127 para float)
+    int expoVies;
+    expoVies = expoente - 127;
+
+    printf("Float:\n");
+    //para adicionar 16bits
+    printf(" %u  %.16fx10^%u \n", sinal, num, expoente);
+    printf("Sinal: %u\n", sinal);
+    printf("Expoente: %u\n", expoente);
+    printf("Expoente com viés: %d\n", expoVies);
+    printf("Fração: %u\n", fracao);
+    printf("\n");
+}
+
+void Double(double num) {
+    //utilizado o unsigned long long pra manter a precisão do double que tem maior precisão em comparação ao float e precisa de mais bits
+    unsigned long long bits;
+    memcpy(&bits, &num, sizeof(double));
+
+    // Sinal
+    unsigned long long sinal = (bits >> 63) & 0x1;
+
+    // Expoente
+    unsigned long long expoente = (bits >> 52) & 0x7FF;
+
+    // Fração
+    unsigned long long fracao = bits & 0xFFFFFFFFFFFFF;
+
+    // Expoente com viés (bias de 1023 para double)
+    long long expoVies = expoente - 1023;
+
+    printf("Double:\n");
+    //para adicionar 16 bits
+    printf(" %llu  %.16fx10^%llu \n", sinal, num, expoente);
+    printf("Sinal: %llu\n", sinal);
+    printf("Expoente: %llu\n", expoente);
+    printf("Expoente com viés: %lld\n", expoVies);
+    printf("Fração: %llu\n", fracao);
+}
+
+int main() {
+    float numFloat;
+    double numDouble;
+
+    printf("digite um número para representação em float: ");
+    scanf("%f", &numFloat);
+    Float(numFloat);
+
+    printf("digite um número para representação em double: ");
+    scanf("%lf", &numDouble);
+    Double(numDouble);
+
+    return 0;
+}
+
+
+
+
+
+
